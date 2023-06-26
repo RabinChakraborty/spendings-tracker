1e931bbd7b0b3fba1c8f028d728ef22c876f6b06
Rabin Chakraborty <rabin747809@gmail.com>
2023-06-26 07:30:27 +0530

    more styling and components added

--- src/components/ExpanseItems.jsx
+++ src/components/ExpanseItems.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './ExpanseItems.css';
 import ExpanseDate from './ExpanseDate';
+import Card from './card';
 
 const ExpanseItems = (props) => {
   const { amount, date, title } = props;
 
   return (
     <>
-      <div className='expense-item'>
+      <Card className='expense-item'>
         <ExpanseDate date={date} />
         <div className='expense-item__description'>
           <h2>{title}</h2>
           <div className='expense-item__price'>${amount}</div>
         </div>
-      </div>
+      </Card>
     </>
   );
 };
