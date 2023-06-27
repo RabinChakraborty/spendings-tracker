b2b69f28800c80de4154ee1c9f468076adefef0d
Rabin Chakraborty <rabin747809@gmail.com>
2023-06-27 13:05:18 +0530

    more updates

--- src/components/Expanses.jsx
+++ src/components/Expanses.jsx
@@ -2,50 +2,29 @@ import ExpanseItems from './ExpanseItems';
 import './Expanses.css';
 import Card from './card';
 
-function Expanses() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+function Expanses(ExpansesData) {
   return (
     <>
-      <Card className='expenses'>
+      <Card className='Expanses'>
         <ExpanseItems
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
+          title={ExpansesData.ExpansesData[0].title}
+          amount={ExpansesData.ExpansesData[0].amount}
+          date={ExpansesData.ExpansesData[0].date}
         />
         <ExpanseItems
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
+          title={ExpansesData.ExpansesData[1].title}
+          amount={ExpansesData.ExpansesData[1].amount}
+          date={ExpansesData.ExpansesData[1].date}
         />
         <ExpanseItems
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
+          title={ExpansesData.ExpansesData[2].title}
+          amount={ExpansesData.ExpansesData[2].amount}
+          date={ExpansesData.ExpansesData[2].date}
         />
         <ExpanseItems
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
+          title={ExpansesData.ExpansesData[3].title}
+          amount={ExpansesData.ExpansesData[3].amount}
+          date={ExpansesData.ExpansesData[3].date}
         />
       </Card>
     </>
